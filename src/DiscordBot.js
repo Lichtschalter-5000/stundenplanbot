be831e78204f4d9aaeadbb27880d409af26843c6
Lichtschalter-5000 <spitzbubenkoenig@web.de>
2021-11-13 13:26:55 +0100

    add DiscordBot.js to last commit

--- src/DiscordBot.js
+++ src/DiscordBot.js
@@ -1,5 +1,6 @@
 const {Client, Intents, MessageEmbed, MessageActionRow, MessageButton} = require("discord.js");
 const fs = require("fs");
+const {DSB_USERNAME, DSB_PASSWORD} = require("../Credentials");
 
 const TOKEN = require("../Credentials").DISCORD_TOKEN;
 
@@ -20,11 +21,39 @@ module.exports = class DiscordBot {
         client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
         client.on('interactionCreate', async interaction => {
-
-            if (interaction.isCommand()) {
+            const id = interaction.inGuild()?interaction.channelId:interaction.user.id;
+            const isAuthed = db.hasOwnProperty(id)?db[id]["authed"]:false;
+            if(!isAuthed && !(interaction.isCommand() && interaction["commandName"] === "auth")) {
+                interaction.reply({content: "Please authorise first: Supply the credentials for DSBMobile via /auth [username] [password]", ephemeral: true}).catch(console.error);
+            } else if (interaction.isCommand()) {
                 switch (interaction["commandName"]) {
                     case "ping": {
-                        await interaction.reply("Pong!");
+                        interaction.reply("Pong!").catch(console.error);
+                        break;
+                    }
+                    case "auth": {
+                        if (isAuthed) {
+                            interaction.reply({content: "You are already authorised, there's no point in doing it over and over again, cutie.", ephemeral: true}).catch(console.error);
+                        } else if (interaction.options.getString("username") === DSB_USERNAME && interaction.options.getString("password") === DSB_PASSWORD) { //ToDo prevent runtime analysis and safely compare
+                            const form = interaction.options.getString("form");
+                            interaction.reply({content: "Great, you are authorised now!", ephemeral: true}).then(() => {
+                                if (db.hasOwnProperty(id)) {
+                                    db[id]["authed"] = true;
+                                } else {
+                                    db[id] = {"channel": interaction.inGuild(), "authed": true};
+                                }
+                                if (form) {
+                                    db[id]["form"] = form;
+                                    return interaction.followUp("Alrighty, from now on you" +
+                                        (interaction.inGuild()?" (you being \"y'all on this server\")":"") +
+                                        " will get updates about the form VT" + form + "!").catch(console.error);
+                                }
+                            }).catch(console.error).finally(() => {
+                                this.updatedb();
+                            });
+                        } else {
+                            interaction.reply({content: "These credentials were not correct.", ephemeral: true}).catch(console.error);
+                        }
                         break;
                     }
                     case "getschedule": {
@@ -48,16 +77,11 @@ module.exports = class DiscordBot {
                         await interaction.reply("Alrighty, from now on you" +
                             (interaction.inGuild()?" (you being \"y'all on this server\")":"") +
                             " will get updates about the form VT" + form + "!");
-                        let id = interaction.inGuild()?interaction.channelId:interaction.user.id;
-                        if (db.hasOwnProperty(id)) {
-                            db[id]["form"] = form;
-                        } else {
-                            db[id] = {"form": form, "channel": interaction.inGuild()};
-                        }
+                        db[id]["form"] = form;
                         this.updatedb();
                         break;
                     case "getlessonstart": {
-                        const form = this.getFormFromInteraction(interaction);
+                        const form = this.getFormFromId(id);
                         if (!form) {
                             await interaction.reply("Well, uhm, would you like to tell me which form you are in? (/setform [form])");
                             // ToDo automatically ask which class to set
@@ -94,7 +118,7 @@ module.exports = class DiscordBot {
                         try {
                             await interaction.deferUpdate();
                             await interaction.editReply({
-                                content: this.parser.getFirstLesson(new Date(), this.getFormFromInteraction(interaction)),
+                                content: this.parser.getFirstLesson(new Date(), this.getFormFromId(id)),
                                 components: []
                             });
                         } catch (e) {
@@ -106,7 +130,7 @@ module.exports = class DiscordBot {
                         try {
                             await interaction.deferUpdate();
                             await interaction.editReply({
-                                content: this.parser.getFirstLesson(this.getTomorrowDate(), this.getFormFromInteraction(interaction)),
+                                content: this.parser.getFirstLesson(this.getTomorrowDate(), this.getFormFromId(id)),
                                 components: []
                             });
                         } catch (e) {
@@ -128,8 +152,7 @@ module.exports = class DiscordBot {
         fs.writeFileSync("./src/discord_db.json", JSON.stringify(db, null, "\t"));
     }
 
-    getFormFromInteraction(interaction) {
-        const id = interaction.inGuild()?interaction.channelId:interaction.user.id;
+    getFormFromId(id) {
         return db.hasOwnProperty(id)?db[id]["form"]:null;
     }
 
@@ -141,10 +164,12 @@ module.exports = class DiscordBot {
 
     async notifyDaily() {
         for (const id in db) {
-            const form = db[id]["form"];
-            const destination = db[id]["channel"]?await client.channels.fetch(id):await client.users.fetch(id);
-            if(this.blockSchedule.getDayInSchedule(form,this.getTomorrowDate())) {
-                await destination.send("Tomorrow, your lessons will start at " + this.parser.getFirstLesson(this.getTomorrowDate(), form));
+            if (db[id]["authed"]) {
+                const form = db[id]["form"];
+                const destination = db[id]["channel"]?await client.channels.fetch(id):await client.users.fetch(id);
+                if (this.blockSchedule.getDayInSchedule(form, this.getTomorrowDate())) {
+                    await destination.send("Tomorrow, your lessons will start at " + this.parser.getFirstLesson(this.getTomorrowDate(), form));
+                }
             }
         }
     }
